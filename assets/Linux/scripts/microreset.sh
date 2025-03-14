24a91c52c94bbca4ba4804a35fbe69e7e4d68aff
Will LE <lexuandinhct@gmail.com>
2025-03-14 11:36:33 +0700

    Auto saved by Logseq

--- assets/Linux/scripts/microreset.sh
+++ assets/Linux/scripts/microreset.sh
@@ -9,17 +9,21 @@ today=$(date '+Y-m-d')
 now=$(date '+H:M:S')
 dt="${today}_${now}"
 
-tension=0; problem=''
+tension=(0); problem=''
 if ! zenity --question --icon-name=emblem-generic --title "${now}" --text "Stop! Breath, Relax..."
 then
-    if ! tension=$(zenity --list --checklist --title "${now}" --text "How much tension's remaining?"\
-        --column "" --column "Level of tension" \
-        true '0' false '10'  false '20' false '30' false '40' false '50' \
-        false '60' false '70' false '80' false '90' false '100' false '♾<fe0f>' \
-        false '+ Comments')
-    then
-        tension=-1
+    if ! tension=($(zenity --list --checklist --title "${now}" --text "How much tension's remaining?"\
+        --separator ' ' --column "" --column "Level of tension" --column ""\
+        true 0 '%' false 10 '%'  false 20 '%' false 30 '%' false 40 '%' false 50 '%' \
+        false 60 '%' false 70 '%' false 80 '%' false 90 '%' false 100 '%' false -1 '♾<fe0f>' \
+        false '+' 'Comments'))
+    then tension=(-1 '+'); fi
+    if [[ ${tension[-1]} == '+' ]]; then
+        [[ ${#tension[@]} -lt 2 ]] && tension=(-1 ${tension[-1]})
+        tension=${tension[-2]}
         problem=$(zenity --entry --width 500 --title "${now}" --text "What's the problem?")
+    else
+        tension=${tension[-1]}
     fi
 fi
 xinput --reattach "$keyboard_id" 3
