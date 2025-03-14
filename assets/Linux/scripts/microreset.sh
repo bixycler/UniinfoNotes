ba46464ed74971ed9696625c8c205577eb038e33
Will LE <lexuandinhct@gmail.com>
2025-03-14 10:36:33 +0700

    Auto saved by Logseq

--- assets/Linux/scripts/microreset.sh
+++ assets/Linux/scripts/microreset.sh
@@ -9,19 +9,18 @@ today=$(date '+Y-m-d')
 now=$(date '+H:M:S')
 dt="${today}_${now}"
 
-keyboard_id=3 # `id=3` in $(xinput --list | grep -i "keyboard")
-xinput --disable "$keyboard_id"
+keyboard_id=9 # `id=3` in $(xinput --list | grep -i "keyboard")
+xinput --float "$keyboard_id"
 
 tension=0; problem=''
 if ! {
     zenity --question --icon-name=emblem-generic --title "${now}" --text "Stop! Breath, Relax..."\
     || tension=$(zenity --list --checklist --title "${now}" --text "How much tension's remaining?"\
         --column "" --column "Level of tension" true '0' false '10'  false '20' false '30' false '40' false '50' false '60' false '70' false '80' false '90' false '100' false '♾<fe0f>' false '+ Comments'); }
-    zenity 
 then
     tension=-1
-    xinput --enable "$keyboard_id"
+    xinput --reattach "$keyboard_id" 3
     problem=$(zenity --entry --width 500 --title "${now}" --text "What's the problem?")
 fi
-xinput --enable "$keyboard_id"
+xinput --reattach "$keyboard_id" 3
 echo -e "${dt}\t${tension}\t${problem}" >> $LOGF
