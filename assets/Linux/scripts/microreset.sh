4b4ca821321e63426f76ef29b6c6728f0678beb9
Will LE <lexuandinhct@gmail.com>
2025-03-14 19:56:33 +0700

    Auto saved by Logseq

--- assets/Linux/scripts/microreset.sh
+++ assets/Linux/scripts/microreset.sh
@@ -12,19 +12,16 @@ dt="${today}_${now}"
 tension=0; problem=''
 if ! zenity --question --icon-name=emblem-generic --title "${now}" --text "Stop! Breath, Relax..."
 then
-    if ! tension=($(zenity --list --checklist --title "${now}" --text "How much tension's remaining?"\
-        --separator=' ' --column "" --column "" --column "" -- \
-        true 0 '%' false 10 '%'  false 20 '%' false 30 '%' false 40 '%' false 50 '%' \
-        false 60 '%' false 70 '%' false 80 '%' false 90 '%' false 100 '%' false ' -1' '♾️' \
-        false '+' 'Comments')) # the space before -1 in ' -1' is to avoid "option" collision, even after `--`!
-    then tension=(-1 '+'); fi
-    if [[ ${tension[-1]} == '+' ]]; then
-        [[ ${#tension[@]} -lt 2 ]] && tension=(-1 ${tension[-1]})
-        tension=${tension[-2]}; tensionStr=${tension}
+    if ! tension=$(zenity --list --title "${now}" --text "How much tension's remaining?"\
+        --height 400 --column "" --column "" -- \
+        0 '%' 10 '%' 20 '%' 30 '%' 40 '%' 50 '%' \
+        60 '%' 70 '%' 80 '%' 90 '%' 100 '%' ' -1' '♾️' )
+        # the space before -1 in ' -1' is to avoid "option" collision, even after `--`!
+    then tension='-1'; fi
+    if [[ ${tension} != '0' ]]; then
+        tensionStr="${tension}%"
         [[ ${tension} == '-1' ]] && tensionStr='♾️' 
         problem=$(zenity --entry --width 500 --title "${now}" --text "What's the problem with tension = ${tensionStr}?")
-    else
-        tension=${tension[-1]}
     fi
 fi
 echo -e "${dt}\t${tension}\t${problem}" >> $LOGF
