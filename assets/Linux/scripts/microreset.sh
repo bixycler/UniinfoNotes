314a70b2d655f3e49df22c2241f3438b0cef1948
Will LE <lexuandinhct@gmail.com>
2025-03-14 10:06:33 +0700

    Auto saved by Logseq

--- assets/Linux/scripts/microreset.sh
+++ assets/Linux/scripts/microreset.sh
@@ -5,14 +5,16 @@ export DISPLAY=':0'
 export DBUS_SESSION_BUS_ADDRESS='unix:path=/run/user/1001/bus' # 1001 = $(id -u dinhlx)
 
 LOGF=${1:-$HOME/Documents/microreset.log}
-today=$(date '+%Y-%m-%d')
-now=$(date '+%H:%M:%S')
+today=$(date '+Y-m-d')
+now=$(date '+H:M:S')
 dt="${today}_${now}"
 
 tension=0; problem=''
 if ! {
     zenity --question --icon-name=emblem-generic --title "${now}" --text "Stop! Breath, Relax..."\
-    || tension=$(zenity --scale --title "${now}" --text "How much tension's remaining?"); }
+    || tension=$(zenity --list --checklist --title "${now}" --text "How much tension's remaining?"\
+        --column "" --column "Level of tension" true '0' false '10'  false '20' false '30' false '40' false '50' false '60' false '70' false '80' false '90' false '100' false '♾<fe0f>' false '+ Comments'); }
+    zenity 
 then
     tension=-1
     problem=$(zenity --entry --width 500 --title "${now}" --text "What's the problem?")
