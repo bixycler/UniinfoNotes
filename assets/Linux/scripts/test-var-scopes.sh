d4fe9ca1a4bfb6d4dfed499326ea13674f9f7873
Will LE <lexuandinhct@gmail.com>
2025-04-09 17:12:31 +0700

    Auto saved by Logseq

--- assets/Linux/scripts/test-var-scopes.sh
+++ assets/Linux/scripts/test-var-scopes.sh
@@ -2,22 +2,22 @@
 lrvar='global lrvar'
 declare_r(){
     echo 'declare_r()-----------'
-    declare -r lrvar='local readonly'
+    declare -r lrvar='local declare -r'
     echo "local lrvar=$lrvar"
     lrvar='locally changed'
     echo "local lrvar=$lrvar" 
 }
 local_then_readonly(){
     echo 'local_then_readonly()-----------'
-    local lrvar='local readonly'
-    readonly lrvar
+    local lrvar='local'
+    readonly lrvar='local then readonly'
     echo "local lrvar=$lrvar"
     lrvar='locally changed'
     echo "local lrvar=$lrvar" 
 }
 readonly_then_local(){
     echo 'readonly_then_local()-----------'
-    readonly lrvar='local readonly'
+    readonly lrvar='readonly in readonly_then_local()'
     local lrvar
     echo "local lrvar=$lrvar"
     lrvar='locally changed'
@@ -32,7 +32,7 @@ local_readonly(){
 }
 readonly_local(){
     echo 'readonly_local()-----------'
-    readonly local lrvar='local readonly'
+    readonly local lrvar='readonly local'
     echo "local lrvar=$lrvar"
     lrvar='locally changed'
     echo "local lrvar=$lrvar" 
--- pages/share/technical/Linux.md
+++ pages/share/technical/Linux.md
@@ -924,7 +924,8 @@ CLOCK: [2024-07-15 Mon 11:04:21]
 						- `readonly` ≈ `declare -r` but *not exactly*!
 							- `readonly var=val` ⇔ `var=val; readonly var`
 								- While `declare -r` declares a new var, global outside function or local inside function, `readonly` only modify the read-only attribute of a declared var.
-							- `readonly local` works but `local readonly` doesn't! 😵‍💫🤪
+							- Neither `readonly local` nor `local readonly` works! 🤪
+								- `readonly local` is *not local*, and `local readonly` is *not read-only*!
 							- Test script: ![test-var-scopes.sh](../assets/Linux/scripts/test-var-scopes.sh)
 				- **Environment variables** are shell vars `export`ed to all **subprocesses** spawned by this shell.
 				  id:: 67f6257e-455e-46f1-8225-8510f60b8b47
