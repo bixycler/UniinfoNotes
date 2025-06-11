fee1d1ea758ba0f15baa56fbf991c18287f030ab
Will LE <lexuandinhct@gmail.com>
2025-06-11 10:36:17 +0700

    Auto saved by Logseq

--- assets/Will/story/2025-06/TelegramSaved_2020-02_2025-05/messages-converter.js
+++ assets/Will/story/2025-06/TelegramSaved_2020-02_2025-05/messages-converter.js
@@ -4,6 +4,7 @@ function groupMessagesByDay() {
     let days = listDiv.querySelectorAll('.message.service');
     for (let day of days) {
         console.log("Processing day: ", day);
-        
+        let date = day.childNodes[0];
+        date.mo
     }
 }
