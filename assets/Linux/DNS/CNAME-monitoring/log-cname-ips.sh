1d08a0368d366d851c63760d68d7f282f50aef2e
Will LE <lexuandinhct@gmail.com>
2025-08-08 18:16:39 +0700

    Auto saved by Logseq

--- assets/Linux/DNS/CNAME-monitoring/log-cname-ips.sh
+++ assets/Linux/DNS/CNAME-monitoring/log-cname-ips.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+githost=mgmt-gitlab-clb-1008603512.ap-northeast-1.elb.amazonaws.com
 hosts=(mgmt-gitlab-clb-1008603512.ap-northeast-1.elb.amazonaws.com
     mgmt-repo1-clb-243604401.ap-northeast-1.elb.amazonaws.com
     mgmt-tools-alb-1633637944.ap-northeast-1.elb.amazonaws.com)
@@ -8,8 +9,8 @@ cd ${HOME}/tmp/
 echo "Monitoring hosts:"
 printf '  %s\n' ${hosts[@]}
 minute=0
+ttl=0
 while true; do
-    echo -n " ${minute}"
     dt=$(date '+%Y-%m-%d_%H:%M:%S')
     for host in ${hosts[@]}; do
         IPs=$(dig +short ${host} | sort)
@@ -30,6 +31,8 @@ while true; do
         fi
     done
     ((minute++))
-    sleep 60
+    ttl=$(dig +noall +answer +ttlid ${githost} | tail -1 | awk '{print $2}')
+    echo -n " ${minute}m+${ttl}s"
+    sleep ${ttl}
 done
 
