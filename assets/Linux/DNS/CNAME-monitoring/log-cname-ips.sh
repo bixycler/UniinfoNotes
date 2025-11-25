cc8cd36836affe129510eb03953ccc8c10790fc9
Will LE <lexuandinhct@gmail.com>
2025-11-25 10:11:59 +0700

    Auto saved by Logseq

--- assets/Linux/DNS/CNAME-monitoring/log-cname-ips.sh
+++ assets/Linux/DNS/CNAME-monitoring/log-cname-ips.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
 githost=mgmt-gitlab-clb-1008603512.ap-northeast-1.elb.amazonaws.com
-hosts=(${githost}
+mgmthosts=(${githost}
     mgmt-repo1-clb-243604401.ap-northeast-1.elb.amazonaws.com
     mgmt-tools-alb-1633637944.ap-northeast-1.elb.amazonaws.com
+)
+dbhosts=( 
     pre1-mastest.c1wbmxxj2vu5.ap-northeast-1.rds.amazonaws.com
     pre1-sgmvtest-20240315.c1wbmxxj2vu5.ap-northeast-1.rds.amazonaws.com
     pre1-myskyg.c1wbmxxj2vu5.ap-northeast-1.rds.amazonaws.com
@@ -13,35 +15,38 @@ hosts=(${githost}
     pre1-domtourdb.c1wbmxxj2vu5.ap-northeast-1.rds.amazonaws.com
     pre1-sessiondb-cluster.cluster-c1wbmxxj2vu5.ap-northeast-1.rds.amazonaws.com
 )
+hosts=(${mgmthosts[@]} ${dbhosts[@]})
+
 logf=log-cname-ips.log
 cd ${HOME}/tmp/
 echo "Monitoring hosts:"
 printf '  %s\n' ${hosts[@]}
-minute=0
-ttl=0
+seconds=0
 while true; do
+    ttls=()
     dt=$(date '+%Y-%m-%d_%H:%M:%S')
     for host in ${hosts[@]}; do
-        IPs=$(dig +short ${host} | sort)
-        IPn=$(printf "${IPs}\n" | wc -l)
-        while [[ ${IPn} -lt 1 ]]; do # retry
+        IPs=(); ttl=0; st="${host}:${dt}"
+        while [[ ${#IPs[@]} -lt 1 ]]; do # retry
             sleep 0.1 # try to avoid the expiration threshold (TTL = 0) 
-            IPs=$(dig +short ${host} | sort)
-            IPn=$(printf "${IPs}" | wc -l)
+            IPs=($(dig +short ${host} | sort))
+            ttl=$(dig +noall +answer +ttlid ${host} | tail -1 | awk '{print $2}')
         done
-        oIPs=$(cat ${host}.ip.log)
+        ttls+=($ttl)
+        oIPs=($(cat ${host}.ip.log))
         if [[ "${IPs}" != "${oIPs}" ]]; then
-            printf "${IPs}" > ${host}.ip.log
-            st="${host}:${dt}"
+            printf "%s\n" "${IPs[@]}" > ${host}.ip.log
             echo "${st}:" ${IPs} >> ${logf}
-            echo -e "\n${st}"; printf '  %s\n' ${IPs}
+            echo -e "\n${st}"; printf "  %s\n" ${IPs[@]}
         else :
-            #echo -e "\n= ${host}:${dt}:" ${IPs} # DEBUG
+            echo -e "\n= ${st}:" ${IPs} # DEBUG
         fi
     done
-    ((minute++))
-    ttl=$(dig +noall +answer +ttlid ${githost} | tail -1 | awk '{print $2}')
-    echo -n " ${minute}m+${ttl}s"
+    ttls=($(printf "%s\n" "${ttls[@]}" | sort -n))
+    echo -e "\n+ TTLs:" ${ttls} # DEBUG
+    ttl=${ttls[0]}
+    ((seconds+=ttl))
+    echo -n " ${seconds}s"
     sleep ${ttl}
 done
 
