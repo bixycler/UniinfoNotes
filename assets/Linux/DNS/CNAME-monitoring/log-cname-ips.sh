e7c46c3d88ca4098befe5ec5fdf83cf45520cdd9
Will LE <lexuandinhct@gmail.com>
2025-11-25 12:31:59 +0700

    Auto saved by Logseq

--- assets/Linux/DNS/CNAME-monitoring/log-cname-ips.sh
+++ assets/Linux/DNS/CNAME-monitoring/log-cname-ips.sh
@@ -2,14 +2,11 @@
 
 # Dig CNAME records for IP to update cname.hosts
 
-# Management hosts have TTL = 1 minute
+githost=git1.lan.skygate.co.jp
 gitcname=mgmt-gitlab-clb-1008603512.ap-northeast-1.elb.amazonaws.com
-declare -A mgmtcnames=([git1.lan.skygate.co.jp]=${gitcname}
+declare -A cnames=([$githost]=${gitcname}
     [repo1.lan.skygate.co.jp]=mgmt-repo1-clb-243604401.ap-northeast-1.elb.amazonaws.com
     [docker-registry-web.dena-travel.internal]=mgmt-tools-alb-1633637944.ap-northeast-1.elb.amazonaws.com
-)
-# DB hosts have very short TTL: 5 seconds!
-declare -A dbcnames=( 
     [dbm.lan.skygate.co.jp]=pre1-mastest.c1wbmxxj2vu5.ap-northeast-1.rds.amazonaws.com
     [view.lan.skygate.co.jp]=pre1-sgmvtest-20240315.c1wbmxxj2vu5.ap-northeast-1.rds.amazonaws.com
     [mymaster.lan.skygate.co.jp]=pre1-myskyg.c1wbmxxj2vu5.ap-northeast-1.rds.amazonaws.com
@@ -21,27 +18,47 @@ declare -A dbcnames=(
     [domtourdb.lan.skygate.co.jp]=pre1-domtourdb.c1wbmxxj2vu5.ap-northeast-1.rds.amazonaws.com
     [sessiondb]=pre1-sessiondb-cluster.cluster-c1wbmxxj2vu5.ap-northeast-1.rds.amazonaws.com
 )
-hosts=(${!mgmtcnames[@]} ${!dbcnames[@]}) 
+# Management hosts have TTL = 1 minute
+mgmthosts=($githost
+    repo1.lan.skygate.co.jp
+    docker-registry-web.dena-travel.internal
+)
+# DB hosts have very short TTL: 5 seconds!
+dbhosts=( 
+    dbm.lan.skygate.co.jp
+    view.lan.skygate.co.jp
+    mymaster.lan.skygate.co.jp
+    mycache.lan.skygate.co.jp
+    mysig-master.lan.skygate.co.jp
+    mysig-slave.lan.skygate.co.jp
+    somali-master.lan.skygate.co.jp
+    somali-slave.lan.skygate.co.jp
+    domtourdb.lan.skygate.co.jp
+    sessiondb
+)
+# All hosts
+hosts=(${mgmthosts[@]} ${dbhosts[@]}) 
 
 logf=dig-cname-ips.log
 cnamehostsf=${HOME}/hosts/active/cname.hosts
 
 cd ${HOME}/tmp/
 echo "Monitoring hosts:"
-printf '  %s\n' ${hosts[@]}
-declare -A cnamehosts
+printf '  %s\n' ${cnames[*]@K}
+declare -A cnameIPs
 seconds=0
 while true; do
-    IPupdates=(); cnamehosts=(); ttls=()
+    IPupdates=(); cnameIPs=(); ttls=()
     dt=$(date '+%Y-%m-%d_%H:%M:%S')
     for host in ${hosts[@]}; do
         IPs=(); ttl=0; st="${host}:${dt}"
-        while [[ ${#IPs[@]} -lt 1 ]]; do # dig $host for IP & TTL
+        # Dig CNAME of $host for IP & TTL
+        while [[ ${#IPs[@]} -lt 1 ]]; do 
             sleep 0.01 # try to avoid the expiration threshold (TTL = 0) 
-            IPs=($(dig +short ${host} | sort))
-            ttl=$(dig +noall +answer +ttlid ${host} | tail -1 | awk '{print $2}')
+            IPs=($(dig +short ${cnames[$host]} 2>/dev/null | sort))
+            ttl=$(dig +noall +answer +ttlid ${cnames[$host]} 2>/dev/null | tail -1 | awk '{print $2}')
         done
-        cnamehosts[$host]=${IPs[0]}
+        cnameIPs[$host]=(${IPs[@]})
         ttls+=($ttl)
         # Check the dug IPs against the stored IPs
         oIPs=($(cat ${host}.ip.log))
@@ -54,12 +71,19 @@ while true; do
             #echo -e "\n= ${st}:" ${IPs[*]} # DEBUG
         fi
     done
-    if [[ ${#IPupdates[@]} -gt 0 ]]; then # update cname.hosts
-        echo '### IPs from CNAME records ###' > cnamehostsf
-        for cname in ${!cnamehosts[@]}; do
 
+    # Update cname.hosts
+    if [[ ${#IPupdates[@]} -gt 0 ]]; then 
+        echo '### IPs from CNAME records ###' > $cnamehostsf
+        for host in ${hosts[@]}; do
+            echo -e "\n"
+            for ip in ${cnameIPs[$host]}; do
+                echo ${ip} ${host} >> $cnamehostsf
+            done
+            echo '#CNAME' ${cnames[$host]} >> $cnamehostsf
         done
     fi
+    # Sleep in TTL
     ttls=($(printf "%s\n" "${ttls[@]}" | sort -n))
     #echo -e "\n+ TTLs:" ${ttls[*]} # DEBUG
     #ttl=${ttls[0]} # the least TTL
