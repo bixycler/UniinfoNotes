4209602a6126ad349092d84d91a63a3221a9dc96
Will LE <lexuandinhct@gmail.com>
2026-05-14 13:56:05 +0700

    Auto saved by Logseq

--- assets/Philosophy/SUM/sensibleuniversemodel.com/tmp_process.js
+++
@@ -1,8 +0,0 @@
-const { processExtractedMarkdown } = require('./process_extracted.js');
-const fs = require('fs');
-
-const inputFile = '/home/dinhlx/.local/share/opencode/tool-output/tool_e2537d261001NCkibN0jY3scbC';
-const outputFile = '/home/dinhlx/source/UniinfoNotes/assets/Philosophy/SUM/sensibleuniversemodel.com/psx-archive/psx-espanol.md';
-
-processExtractedMarkdown(inputFile, outputFile);
-console.log('Processed psx-espanol.md');
